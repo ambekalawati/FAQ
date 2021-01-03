6a7dc5d9af0235c7e24ccdce683f5b38ce0b0764
ambekalawati <kalawati.ambe12@gmail.com>
2021-01-03 07:45:05 +0530

    Arrays

---
+++ OCA/src/arrays/Q1.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Q1 {
+	static int i=1;
+
+	public static void main(String[] args) {
+		int array[] = new int[] { 10, 12, 13 };
+		System.out.println(array[i++]);
+
+	}
+
+}
